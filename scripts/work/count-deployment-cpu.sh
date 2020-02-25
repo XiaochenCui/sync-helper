4ffff3539871e4a6e1df830f792df1fa5c242b50
XiaochenCui <jcnlcxc.new@gmail.com>
2020-02-25 15:07:51 +0800

    update

--- scripts/work/count-deployment-cpu.sh
+++ scripts/work/count-deployment-cpu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in {1..30}
+for i in {1..12}
 do
     cpu=$(kubectl top pod | grep $1 | awk '{ SUM += $2} END {print SUM}')
     memory=$(kubectl top pod | grep $1 | awk '{ SUM += $3} END {print SUM}')
