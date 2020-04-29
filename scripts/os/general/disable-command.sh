7030bb1cbc0bb20e0cd8a38ceb3c9136a239d6a9
XiaochenCui <jcnlcxc.new@gmail.com>
2020-04-29 11:00:09 +0800

    update

--- scripts/os/general/disable-command.sh
+++ scripts/os/general/disable-command.sh
@@ -1,2 +1,23 @@
 command=$1
-which $command
+origin_location=$(which $command)
+
+echo "源地址为：$origin_location"
+
+# 查看指令是否存在
+if [[ "$origin_location" == "" ]]; then
+    echo "command '$command' not found, exit"
+    exit 1
+fi
+
+# 查看指令是否是 symbolink
+if [[ -L "$origin_location" ]]; then
+    echo "command '$command' is symbolink, exit"
+    exit 1
+fi
+
+backup_location="$origin_location.bak"
+echo "备份地址为：$backup_location"
+
+# 备份
+cp $origin_location $backup_location
+rm $origin_location
